c20dbc2086a1d8e19b1fa0d9d1a50a06755cac5c
Andreas Dranidis <adranidis@trigonongroup.com>
2025-01-25 11:19:45 +0200

    Remove components

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import PokemonList from "./PokemonList.tsx";
 import { MantineProvider } from "@mantine/core";
 
 import "@mantine/core/styles.css";
-import { BrowserRouter, Routes, Route } from "react-router";
-import { AppLayout } from "./AppLayout.tsx";
-import PokemonDetail from "./PokemonDetail.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MantineProvider defaultColorScheme="auto">
-      <BrowserRouter>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route path="/" element={<PokemonList />} />
-            <Route path=":id" element={<PokemonDetail />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </MantineProvider>
+    <MantineProvider defaultColorScheme="auto"></MantineProvider>
   </StrictMode>
 );
